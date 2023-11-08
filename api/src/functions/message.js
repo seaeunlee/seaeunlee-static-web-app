7ba56270a623e2fbd77693f720794b89ca9dacd6
Sea Eun Lee <seaeunlee@microsoft.com>
2023-11-08 14:27:02 -0800

    add filter back in

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -6,8 +6,11 @@ app.http('message', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
+        // Check if the URL contains "api/subscription"
+        if (request.url.includes('api/subscriptions')) {
+            return { status: 200, body: 'OK' };
+        } else {
+            return { status: 404, body: 'Not Found' };
+        }
     }
 });
