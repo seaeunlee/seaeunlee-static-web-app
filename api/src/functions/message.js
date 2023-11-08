d8f473bcae511a21eca875638f801f0e98364a70
Sea Eun Lee <seaeunlee@microsoft.com>
2023-11-08 15:13:57 -0800

    wildstar

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -1,6 +1,6 @@
 const { app } = require('@azure/functions');
 
-app.http('', {
+app.http('*', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
@@ -8,7 +8,7 @@ app.http('', {
 
         // Check if the URL contains "api/subscription"
         if (request.url.includes('api/subscription')) {
-            return { status: 200, body: 'OK 1' };
+            return { status: 200, body: 'OK 2' };
         } else {
             return { status: 404, body: 'Not Found' };
         }
